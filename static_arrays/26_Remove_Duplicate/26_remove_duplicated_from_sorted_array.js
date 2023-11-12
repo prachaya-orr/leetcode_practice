65c0fd60473fe41abc2df1cd18940bd6c98382da
Prachaya Orraeiam <prachaya.or@bdms.co.th>
2023-11-12 22:25:17 +0700

    add new fn

--- static_arrays/26_Remove_Duplicate/26_remove_duplicated_from_sorted_array.js
+++ static_arrays/26_Remove_Duplicate/26_remove_duplicated_from_sorted_array.js
@@ -22,3 +22,37 @@ var removeDuplicates = (nums) => {
 
     return (left + 1);
 };
+
+var removeDuplicates = function (nums) {
+    let [left, right] = [0, 0];
+
+    for (right = 1; right < nums.length; right++) {
+        const [leftVal, rightVal] = [nums[left], nums[right]]
+        if (rightVal !== leftVal) {
+            left++;
+            nums[left] = rightVal;
+        }
+    }
+    return {
+        name: (left + 1),
+        nums
+    };
+};
+
+var removeDuplicates = (nums) => {
+    let [left, right] = [0, 0];
+
+    while (right < nums.length) {
+        const [leftVal, rightVal] = [nums[left], nums[right]];
+
+        const isEqual = (rightVal === leftVal);
+        if (!isEqual) {
+            left++;
+            nums[left] = rightVal;
+        }
+
+        right++;
+    }
+
+    return (left + 1);
+};
